faf4b7a960346de4894b920f425669100e0155ef
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2007-06-20 13:40:06 +0000

    back-porting to 1_3_X

--- interface/CandDecaySelector.h
+++ interface/CandDecaySelector.h
@@ -31,7 +31,7 @@ namespace helper {
     reco::CandidateRef add( reco::CandidateRefProd cands, const reco::Candidate & c ) {
       using namespace reco;
       using namespace std;
-      std::auto_ptr<CompositeRefCandidate> cmp( new CompositeRefCandidate( c ) );
+      std::auto_ptr<CompositeRefCandidate> cmp( new CompositeRefCandidate( c.charge(), c.p4(), c.vertex() ) );
       CompositeRefCandidate * p = cmp.get();
       CandidateRef ref( cands, selCands_->size() );
       selCands_->push_back( cmp );
